605682bc95e4d429c44b48ee0f78782ba9843615
cocal0505 <cocal3250@gmail.com>
2021-06-25 16:46:13 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <h1>{{ count }}</h1>
+</template>
+
+
+<script>
+export default {
+  data() {
+    return {
+      count: 0,
+    };
+  },
+};
+</script>
+
+
+
+<style lang="sass">
+</style>
+
